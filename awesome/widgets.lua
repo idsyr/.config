7755c3424586f0c395aeec4737aab7db91557445
idsyr <idsyr@mail.ru>
2025-09-02 15:46:21 +0500

    awesome: widgets.lua: skeuomorphic taglist, refactoring

--- awesome/widgets.lua
+++ awesome/widgets.lua
@@ -9,20 +9,133 @@ local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
 require("awful.hotkeys_popup.keys")
 
+-- ---------------------------------------------------
+--  UTILS
+-- ---------------------------------------------------
+
+local function mix(color1, color2, ratio)
+   ratio = ratio or 0.5
+   local r1, g1, b1 = color1:match("#(%x%x)(%x%x)(%x%x)")
+   local r2, g2, b2 = color2:match("#(%x%x)(%x%x)(%x%x)")
+   
+   r1, g1, b1 = tonumber(r1, 16), tonumber(g1, 16), tonumber(b1, 16)
+   r2, g2, b2 = tonumber(r2, 16), tonumber(g2, 16), tonumber(b2, 16)
+   
+   local r = math.floor(r1 * (1 - ratio) + r2 * ratio)
+   local g = math.floor(g1 * (1 - ratio) + g2 * ratio)
+   local b = math.floor(b1 * (1 - ratio) + b2 * ratio)
+   
+   return string.format("#%02X%02X%02X", r, g, b)
+end
+
+
+local function get_rounded_rect(roundess)
+   local rounded_rect = function(cr, w, h)
+      gears.shape.rounded_rect(cr, w, h, roundess)
+   end
+   return rounded_rect
+end
+
+local function get_volumetric_border_gradient(height, width)
+   local volumetric_border_gradient = {
+      type = "linear",
+      from = {0, 0},
+      to =  {height, width},
+      stops = {
+	 {0, "#FFFFFFAA"},
+	 {0.1, "#FFFFFFAA"},
+	 {1, "#222222AA"}
+      }
+   }
+   return volumetric_border_gradient
+end
+
+local function get_volumetric_plane_gradient(x, y, color)
+   local volumetric_plane_gradient = {
+      type = "linear",
+      from = {0, 0},
+      to = {x, y},
+      stops = {
+	 {0, mix(color, "#FFFFFF", 0.3)},
+	 {0.5, color},
+	 {1, mix(color, "#000000", 0.3)}
+      }
+   }
+   return volumetric_plane_gradient 
+end
+
+function set_wallpaper(s)
+   -- Wallpaper
+   if beautiful.wallpaper then
+      local wallpaper = beautiful.wallpaper
+      -- If wallpaper is a function, call it with the screen
+      if type(wallpaper) == "function" then
+	 wallpaper = wallpaper(s)
+      end
+      gears.wallpaper.maximized(wallpaper, s, true)
+   end
+end
+
 
 -- ---------------------------------------------------
---  WIDGETS ( EACHSCREEN )
+--  KEYBOARD LAYOUT
 -- ---------------------------------------------------
+mykeyboardlayout_wrapped =
+   awful.widget.keyboardlayout
 
-mykeyboardlayout = awful.widget.keyboardlayout()
-mytextclock      = wibox.widget.textclock()
+-- wibox.widget {
+--    { widget = wibox.container.margin,
+--      left = 1.5,
+--      right = 1.5,
+--      awful.widget.keyboardlayout,
+--    },
+--    widget = wibox.container.background,
 
+--    border_width = 1,
+--    border_color = get_volumetric_border_gradient(40,30),
+
+--    shape = get_rounded_rect(10),
+
+--    bg = get_volumetric_plane_gradient(10, 30, beautiful.taglist_bg_occupied),
+-- }
+
+
+-- ---------------------------------------------------
+--  CLOCK
+-- ---------------------------------------------------
+mytextclock_wrapped =
+   wibox.widget.textclock
+
+-- wibox.widget {
+--    { widget = wibox.container.margin,
+--      left = 1.5,
+--      right = 1.5,
+--      wibox.widget.textclock,
+--    },
+--    widget = wibox.container.background,
+
+--    shape = get_rounded_rect(10),
+
+--    border_width = 1,
+--    border_color = get_volumetric_border_gradient(40, 30),
+
+--    bg = get_volumetric_plane_gradient(5, 40, beautiful.taglist_bg_occupied),
+-- }
+
+
+-- ---------------------------------------------------
+--  SEPARATOR
+-- ---------------------------------------------------
 separator = wibox.widget.separator {
    orientation = 'vertical',
-   forced_width = 21,
+   forced_width = 5,
+   color = "#FF00FF00"
 }
 
 
+-- ---------------------------------------------------
+--  MENU
+-- ---------------------------------------------------
 myawesomemenu = {
    { "hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
    { "manual", terminal .. " -e man awesome" },
@@ -45,7 +158,9 @@ mylauncher = awful.widget.launcher(
 menubar.utils.terminal = terminal
 
 
-
+-- ---------------------------------------------------
+--  TASKLIST
+-- ---------------------------------------------------
 local tasklist_buttons = gears.table.join(
    awful.button({ }, 1, function (c)
 	 if c == client.focus then
@@ -69,15 +184,19 @@ local tasklist_buttons = gears.table.join(
 end))
 
 local function create_tasklist(s)
-    return awful.widget.tasklist {
-        screen  = s,
-        filter  = awful.widget.tasklist.filter.currenttags,
-        buttons = tasklist_buttons
-    }
+   local tasklist = awful.widget.tasklist {
+      screen  = s,
+      filter  = awful.widget.tasklist.filter.currenttags,
+      buttons = tasklist_buttons
+   }
+   return 
+      tasklist
 end
 
 
-
+-- ---------------------------------------------------
+--  TAGLIST
+-- ---------------------------------------------------
 local taglist_buttons = gears.table.join(
    awful.button({ }, 1, function(t) t:view_only() end),
    awful.button({ modkey }, 1, function(t)
@@ -95,35 +214,106 @@ local taglist_buttons = gears.table.join(
    awful.button({ }, 5, function(t) awful.tag.viewprev(t.screen) end)
 )
 
+
+beautiful.taglist_shape = function(cr, w, h)
+   gears.shape.rounded_rect(cr, w, h, 10 )
+end
+
+beautiful.taglist_font = beautiful.font
+beautiful.taglist_spacing = beautiful.useless_gap
+
+beautiful.taglist_bg_focus = {
+   type = "linear",
+   from = {10, 30},
+   to = {0, 0},
+   stops = {
+      {0, mix(beautiful.bg_focus, "#FFFFFF", 0.9)},
+      {0.5, beautiful.bg_focus},
+      {1, mix(beautiful.bg_focus, "#000000", 0.4)},
+   }
+}
+
+beautiful.taglist_bg_occupied = {
+   type = "linear",
+   from = {0, 0},
+   to = {10, 30},
+   stops = {
+      {0, mix(beautiful.taglist_bg_occupied, "#FFFFFF", 0.3)},
+      {0.5, beautiful.taglist_bg_occupied},
+      {1, mix(beautiful.taglist_bg_occupied, "#000000", 0.3)}
+   } 
+}
+
+beautiful.taglist_bg_empty = {
+   type = "linear",
+   from = {0, 0},
+   to = {0, 15},
+   stops = {
+      {0, mix(beautiful.taglist_bg_empty, "#FFFFFF", 0.05)},
+      {1, beautiful.taglist_bg_empty}
+   }
+}
+
+beautiful.taglist_shape_border_width = 1
+
+beautiful.taglist_shape_border_color = {
+   type = "linear",
+   from = {0, 0},
+   to = {40, 30},
+   stops = {
+      {0, "#FFFFFF"},
+      {0.1, "#FFFFFF"},
+      {1, "#000000"}
+   }
+}
+beautiful.taglist_shape_border_color_focus = {
+   type = "linear",
+   from = {0, 0},
+   to = {40, 30},
+   stops = {
+      {0, "#FFFFFF"},
+      {0.1, "#FFFFFF"},
+      {1, "#000000"}
+   }
+}
+
+beautiful.taglist_shape_border_color_occupied = beautiful.taglist_bg_occupied 
+beautiful.taglist_shape_border_color_empty = "#888888"
+beautiful.taglist_shape_border_color_urgent = "#FF5555"
+
 local function create_taglist(s, filter, buttons)
-    return awful.widget.taglist {
-        screen  = s,
-        filter  = filter or awful.widget.taglist.filter.all,
-        buttons = buttons or taglist_buttons
-    }
+   return awful.widget.taglist {
+      screen  = s,
+      filter  = awful.widget.taglist.filter.noempty,
+      buttons = buttons or taglist_buttons,
+      shadow = true,
+      widget_template = {
+	 {
+	    {
+	       id = 'text_role',
+	       widget = wibox.widget.textbox,
+	    },
+	    margins = 8,
+	    widget = wibox.container.margin,
+	 },
+	 id = 'background_role',
+	 widget = wibox.container.background,
+	 shape = beautiful.taglist_shape,
+	 
+      },
+   }
 end
 
 
-
-function set_wallpaper(s)
-   -- Wallpaper
-   if beautiful.wallpaper then
-      local wallpaper = beautiful.wallpaper
-      -- If wallpaper is a function, call it with the screen
-      if type(wallpaper) == "function" then
-	 wallpaper = wallpaper(s)
-      end
-      gears.wallpaper.maximized(wallpaper, s, true)
-   end
-end
-
-
-
+-- ---------------------------------------------------
+--  EACHSCREEN
+-- ---------------------------------------------------
 awful.screen.connect_for_each_screen(function(s)
       set_wallpaper(s)
       awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
 
       s.mylayoutbox = awful.widget.layoutbox(s)
+      
       s.mylayoutbox:buttons(
 	 gears.table.join(
 	    awful.button({ }, 1, function () awful.layout.inc( 1) end),
@@ -135,7 +325,7 @@ awful.screen.connect_for_each_screen(function(s)
       s.mytasklist = create_tasklist(s) 
       s.mypromptbox = awful.widget.prompt()
 
-      s.mywibox = awful.wibar({ position = "top", screen = s })
+      s.mywibox = awful.wibar({ position = "top", screen = s, })
 
       s.mywibox:setup {
 	 layout = wibox.layout.align.horizontal,
@@ -149,8 +339,8 @@ awful.screen.connect_for_each_screen(function(s)
             layout = wibox.layout.fixed.horizontal,
             wibox.widget.systray(), separator,
 	    s.mytaglist,            separator,
-            mykeyboardlayout,       separator,
-	    mytextclock,            separator,
+            mykeyboardlayout_wrapped,       separator,
+	    mytextclock_wrapped,            separator,
             s.mylayoutbox,          separator,
             s.mypromptbox,
 	    -- mylauncher,             separator
